861bee01fc1b9bf418f39b52f44b973ae109dd4d
basshamut <bloodbahamut@gmail.com>
2026-07-13 21:27:13 +0200

    fix: remove iframe sandbox that broke ok.ru player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -36,7 +36,6 @@ export default function VideoPlayer({ episode }) {
           className="w-full h-full border-0"
           allowFullScreen
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          sandbox="allow-scripts allow-same-origin allow-presentation"
           title={episode.title}
         />
       </div>
